c36000be7a941af5193279b4ec52f3e687d2237c
Om Kesalkar <175115527+omkesalkar@users.noreply.github.com>
2025-01-16 18:20:43 +0530

    Add files via upload

---
+++ CollectionExample.java
@@ -0,0 +1,160 @@
+package Jan_08_Set_Collection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		TreeSet<String> tree = new TreeSet<>();
+//		tree.add("25");
+//		tree.add("Naresh");
+//		tree.add("A");
+//		tree.add("37");
+//		for(String str: tree)
+//			System.out.print(str+" ");
+//
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args)
+//	{
+//		Set<String> tree = new TreeSet<>();
+//		tree.add("25");
+//		tree.add("Naresh");
+//		tree.add("A");
+//		tree.add("37");
+//		for(String str: tree)
+//			System.out.print(str+" ");
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		Set<String> tree = new TreeSet<>();
+//		tree.add("A");
+//		tree.add("25");
+//		tree.add("Naresh");
+//		tree.add("37");
+//		tree.add("A");
+//		tree.add("37");
+//		for (String str : tree)
+//			System.out.print(str + " ");
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		Set<String> tree = new TreeSet<>();
+//		tree.add("google");
+//		tree.add("Naresh");
+//		tree.add("Information");
+//		tree.add("Technology");
+//		for(int i=2;i<tree.size();i++) 
+//		{
+//			System.out.print(tree.remove(i));
+//		}
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		Set<String> tree = new TreeSet<>();
+//		tree.add("google");
+//		tree.add("Naresh");
+//		tree.add("Information");
+//		tree.add("Technology");		
+//		for(String str: tree) 
+//		{
+//			if(tree.contains("Naresh"))
+//				System.out.print(str+" ");
+//		}
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		HashSet hash = new HashSet<>();
+//		hash.add("nit");
+//		hash.add("NIT");
+//		System.out.println(hash);
+//		System.out.println(hash.size());
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		HashSet hash = new HashSet<>();
+//		hash.add("nit");
+//		hash.add("nit");
+//		System.out.println(hash);
+//		System.out.println(hash.size());
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args)
+//	{
+//		HashSet set = new HashSet<>();
+//		set.add(new StringBuffer("nit"));
+//		set.add(new StringBuffer("nit"));
+//		System.out.println(set);
+//		System.out.println(set.size());
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args)
+//	{
+//		TreeSet tree = new TreeSet<>();
+//		tree.add("nit");
+//		tree.add("Naresh");
+//		//tree.add(25);    //comment this line coz in TreeSet includes comparable and comparator 
+//		//because gives output in ascending order basis of Hashcode value of String
+//		System.out.println(tree.tailSet("a"));
+//		System.out.println(tree);
+//	}
+//}
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		TreeSet tree = new TreeSet<>();
+//		tree.add("nit");
+//		tree.add("Naresh");
+//		tree.add("Technology");
+//		System.out.println(tree.tailSet("a"));
+//	}
+//}
+
+
+//public class CollectionExample 
+//{
+//	public static void main(String[] args) 
+//	{
+//		TreeSet tree = new TreeSet();
+//		tree.add("nit");
+//		tree.add("Technology");
+//		tree.add("Naresh");
+//		System.out.println(tree);
+//	}
+//}
+
